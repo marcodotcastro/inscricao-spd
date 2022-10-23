05e8b19683e47eb2e0b36ab071c7784190c7f58b
Marco Castro <marcodotcastro@gmail.com>
2022-10-22 21:21:54 -0300

    feat: add create subscripts

--- app/controllers/api/v1/subscripts_controller.rb
+++ app/controllers/api/v1/subscripts_controller.rb
@@ -2,5 +2,19 @@ class Api::V1::SubscriptsController < ApplicationController
     def index
         render json: Subscript.all
     end
+
+    def create
+        @subscript = Subscript.new(subscript_params)
+       
+        if @subscript.save
+          render json: @subscript, status: :created
+        end
+    end
+       
+    private
+       
+    def subscript_params
+        params.require(:subscript).permit(:name, :email, :phone, :description, :language, :schooling)
+    end
        
 end
